92ac8a1c0c705e57432164d6909d9a5ba3b5d716
Matias Aniñir <107155398+MatiasAni23@users.noreply.github.com>
2025-02-11 09:37:33 -0300

    Update Grabacion_ventanas.py

--- Grabacion_ventanas.py
+++ Grabacion_ventanas.py
@@ -188,7 +188,7 @@ class Pruebas_retail(unittest.TestCase):
             "-f", "gdigrab", # Formato de captura de pantalla de Windows
             "-i", f"hwnd={hwnd}", # Pasar el HWND como entrada (Identificador de la ventana a grabar)
             "-framerate", "30", # Frecuencia de cuadros por segundo (FPS)
-            "-s", "1920x1080" # Resolución de video (Reducción de calidad: 1280x720; 854x480)
+            "-s", "1920x1080", # Resolución de video (Reducción de calidad: 1280x720; 854x480)
             "-vf", (
                 f"drawtext=textfile={self.overlay_text_file}:" # Texto con las acciones
                 f"x='main_w-text_w-10':y='main_h-text_h-50':"  # Texto en esquina inferior derecha
